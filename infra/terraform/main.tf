8ccbf1783e3d621cbd042af0b21948c8b9dee2c2
kato0209 <kato.921002@gmail.com>
2024-01-16 22:29:55 +0900

    tf init

---
+++ infra/terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.1.0"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      application = "ReadersLounge"
+    }
+  }
+}
