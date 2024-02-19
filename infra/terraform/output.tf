21a30f593fed9dafbf6ea809650e9457490d042c
kato0209 <kato.921002@gmail.com>
2024-02-19 22:36:54 +0900

    fix flow

--- infra/terraform/output.tf
+++ infra/terraform/output.tf
@@ -1,4 +1,15 @@
+output "subnet_1a_id" {
+  value = aws_subnet.public_1a.id
+}
+
+output "subnet_1c_id" {
+  value = aws_subnet.public_1c.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.ecs_api.id
+}
+
 output "task_definition_arn" {
   value = aws_ecs_task_definition.goose_migration.arn
 }
-
