7342296106a91157373e807ba441f493af16b7b7
kato0209 <kato.921002@gmail.com>
2024-02-19 21:41:23 +0900

    fix migration flow2

--- infra/terraform/output.tf
+++ infra/terraform/output.tf
@@ -1,11 +1,4 @@
-output "subnet_1a_id" {
-  value = aws_subnet.public_1a.id
+output "task_definition_arn" {
+  value = aws_ecs_task_definition.goose_migration.arn
 }
 
-output "subnet_1c_id" {
-  value = aws_subnet.public_1c.id
-}
-
-output "security_group_id" {
-  value = aws_security_group.ecs_api.id
-}
