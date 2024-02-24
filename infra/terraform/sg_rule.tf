2136976944787fe2ad5426c550c616e4c1095fe0
kato0209 <kato.921002@gmail.com>
2024-02-24 23:04:15 +0900

    fix sg_rule

--- infra/terraform/sg_rule.tf
+++ infra/terraform/sg_rule.tf
@@ -81,6 +81,7 @@ resource "aws_security_group_rule" "ecs_front2" {
   from_port                = 80
   to_port                  = 80
   protocol                 = "tcp"
+  cidr_blocks              = ["0.0.0.0/0"]
   source_security_group_id = aws_security_group.alb.id
   security_group_id        = aws_security_group.ecs_front.id
 }
@@ -90,6 +91,7 @@ resource "aws_security_group_rule" "ecs_front3" {
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
+  cidr_blocks              = ["0.0.0.0/0"]
   source_security_group_id = aws_security_group.alb.id
   security_group_id        = aws_security_group.ecs_front.id
 }
@@ -101,6 +103,7 @@ resource "aws_security_group_rule" "ecs_api1" {
   from_port                = 8080
   to_port                  = 8080
   protocol                 = "tcp"
+  cidr_blocks              = ["0.0.0.0/0"]
   source_security_group_id = aws_security_group.alb.id
   security_group_id        = aws_security_group.ecs_api.id
 }
